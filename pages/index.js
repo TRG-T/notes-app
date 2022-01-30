657e4d75e84562221120b067759a9322cf0e9db6
TARGEToff <lockontargt@gmail.com>
2022-01-30 11:05:33 +0100

    added formik and axios

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,32 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import axios from "axios"
+import { useFormik } from "formik";
 
 export default function Home() {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: ({ username, password }) => {
+      axios.post("http://localhost:3001/login", {username, password})
+    }
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.login}>
         <h2>Login</h2>
-        <div className={styles.inputs}>
-          <input placeholder="nickname" type="text" />
-          <input placeholder="password" type="password"/>
-        </div>
-        <button className={styles.button}>Login</button>
-        <p className={styles.register}>Don&apos;t have an account? <a>Register</a></p>
+        <form onSubmit={formik.handleSubmit} className={styles.inputs}>
+          <input id="username" name="username" placeholder="username" type="text" onChange={formik.handleChange} value={formik.values.username} />
+          <input id="password" name="password" placeholder="password" type="password" onChange={formik.handleChange} value={formik.values.password} />
+          <button type="submit" className={styles.button}>Login</button>
+        </form>
+        <p className={styles.register}>
+          Don&apos;t have an account? <a>Register</a>
+        </p>
       </div>
     </div>
   );
