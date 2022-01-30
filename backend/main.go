16d6bd9c9af7580bfceffd2e6a3761de9488f72d
TARGEToff <lockontargt@gmail.com>
2022-01-30 10:31:40 +0100

    added gorm

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type User struct {
+	gorm.Model	
+	Username            string
+	Password            string
+}
+var users []User
+
+
 func main() {
+	godotenv.Load(".env")
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbURI := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432", dbHost, dbUser, dbPassword, dbName)
+	
+	db, err := gorm.Open(postgres.Open(dbURI), &gorm.Config{})
+	db.AutoMigrate(&User{})
+
+	if err != nil {
+		fmt.Println("Connecting to database failed")
+	} else {
+		fmt.Println("Connected to database")
+	}
+	
+	db.Table("users").Find(&users)
+	for _, user := range users {
+		fmt.Println(user.Username, user.Password)
+	}
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "ping",
 		})
 	})
-	r.Run(":3001")
+	r.Run("127.0.0.1:3001")
+	
 }
-
